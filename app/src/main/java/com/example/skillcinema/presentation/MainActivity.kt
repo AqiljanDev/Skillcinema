6456050d19f3cc23fbfb4e287878ae3b0143e5d3
Aqiljan Sayfullaev <akilsayfullaev@gmail.com>
2024-01-18 18:29:21 +0600

    Gone BottomNavigationView

--- app/src/main/java/com/example/skillcinema/presentation/MainActivity.kt
+++ app/src/main/java/com/example/skillcinema/presentation/MainActivity.kt
@@ -2,12 +2,32 @@ package com.example.skillcinema.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.navigation.fragment.NavHostFragment
 import com.example.skillcinema.R
+import com.example.skillcinema.databinding.ActivityMainBinding
 import com.example.skillcinema.presentation.onBoarding.NavigationFragment
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.action_homepage -> {
+                    navController.navigate(R.id.homepageFragment)
+                    return@setOnItemSelectedListener true
+                }
+
+                else -> false
+            }
+        }
     }
 }
